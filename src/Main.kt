0c55684357744ad4c482e38b2cb39880082b21e7
irolram <ivanr9016@gmail.com>
2025-01-21 16:37:32 +0100

    60% hehco

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+class Persona(var peso: Double, var altura:Double,var nombre:String){
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+    val imc: Double
+        get() { //hago un get para poder acceder a él, pero no le hago set para que no se pueda modificar
+            return peso/(altura*altura)
+        }
+    init {
+        require(peso>0) {"El peso debe ser mayor que 0"}
+        require(altura in 0.0..2.5) {"La altura debe ser mayor que 0 y menor de 2,5"}
     }
+
+
+    constructor(nombre:String, peso: Double, altura: Double) : this(peso, altura) {
+        this.nombre = nombre
+
+
+    }
+    override fun toString():String{
+        return "Nombre:$nombre peso: $peso, altura: $altura"
+
+
+    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Persona) return false
+        return nombre == other.nombre && peso == other.peso && altura == other.altura
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+        TODO("HACERLO")
+    }
+
+    fun pedir_nombre(): String {
+        print("Ingrese su nombre: ")
+        val nombre = readLine()!!
+        if (nombre.isEmpty()) return pedir_nombre()
+        return nombre
+    }
+
+    fun saludar() {
+        return println("Hola ${nombre}")
+    }
+    fun alturaEncimaMedia():Boolean{
+        if (altura < 1.75){
+            return false
+        }else{
+            return true
+        }
+    }
+    fun pesoEncimaMedia():Boolean{
+        if(peso < 70.0){
+            return false
+        }else{
+            return true
+        }
+    }
+    fun ObtenerDescImc(){
+
+    }
+}
+
+
+
+fun main() {
+    val persona1 = Persona(60.0,1.50,"Ivan")
+    persona1.saludar()
+    persona1.alturaEncimaMedia()
 }
